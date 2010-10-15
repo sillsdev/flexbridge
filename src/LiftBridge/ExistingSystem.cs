4c265386e683366176ba06a2db86eea43370da34
Randy Regnier <rbregnier@gmail.com>
2010-10-15 10:26:00 -0500

    Add all tab pages to main tab control (except "About", whatever that is supposed to be).

--- src/LiftBridge/ExistingSystem.cs
+++ src/LiftBridge/ExistingSystem.cs
@@ -1,21 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Drawing;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
+using Chorus;
 using Chorus.UI.Sync;
 
-// TODO: It would be really nice to have two events come out of the S/R btn that is clicked.
-// TODO: The first would be done right before the S/R, so the export could be done.
-// TODO: The second would then be raised, right after the S/R, but only if merging was done.
-// TODO: That would optionally allow the import back to FW.
 namespace SIL.LiftBridge
 {
 	public partial class ExistingSystem : UserControl
 	{
+		private readonly ChorusSystem _chorusSystem;
 		private readonly BridgeSyncControl _bridgeControl;
 		private ILiftBridgeImportExport _importerExporter;
 
@@ -24,10 +15,15 @@ namespace SIL.LiftBridge
 			InitializeComponent();
 		}
 
-		public ExistingSystem(BridgeSyncControl bridgeControl)
+		public ExistingSystem(ChorusSystem chorusSystem, BridgeSyncControl bridgeControl)
 			: this()
 		{
+			_chorusSystem = chorusSystem;
 			_bridgeControl = bridgeControl;
+			_tpSendReceive.Controls.Add(bridgeControl);
+			_tpNotes.Controls.Add(_chorusSystem.WinForms.CreateNotesBrowser());
+			_tpHistory.Controls.Add(_chorusSystem.WinForms.CreateHistoryPage());
+			//_tpAbout
 		}
 
 		internal ILiftBridgeImportExport ImporterExporter
@@ -43,17 +39,17 @@ namespace SIL.LiftBridge
 			}
 		}
 
-		void BridgeControl_SyncFinished(object sender, SyncFinishedEventArgs e)
-		{
-			if (e.Results.DidGetChangesFromOthers)
-				_importerExporter.ImportLexicon(FindForm()); // NB: It will use the LiftPathname provided in the exporter handler.
-		}
-
 		void BridgeControl_SyncStarting(object sender, SyncStartingEventArgs e)
 		{
 			_importerExporter.LiftPathname = e.LiftPathname;
 			// ExportLexicon returns 'true' for success, so go with opposite.
 			e.Cancel = !_importerExporter.ExportLexicon(FindForm());
+		}
+
+		void BridgeControl_SyncFinished(object sender, SyncFinishedEventArgs e)
+		{
+			if (e.Results.DidGetChangesFromOthers)
+				_importerExporter.ImportLexicon(FindForm()); // NB: It will use the LiftPathname provided in the exporter handler.
 		}
 	}
 }
