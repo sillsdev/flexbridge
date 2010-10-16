da3f2c9190ca2bbcaeb8f56caf58678fbd190941
Randy Regnier <rbregnier@gmail.com>
2010-10-16 11:20:09 -0500

    Use shared version number for all three assemblies (from version.cs).

--- src/LiftBridge/Properties/AssemblyInfo.cs
+++ src/LiftBridge/Properties/AssemblyInfo.cs
@@ -21,15 +21,16 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("7850cfda-f790-43ed-8597-cdf0e08f8ae2")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.*")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+// The version number now comes from the shared Version.cs file.
+//// Version information for an assembly consists of the following four values:
+////
+////      Major Version
+////      Minor Version
+////      Build Number
+////      Revision
+////
+//// You can specify all the values or you can default the Build and Revision Numbers
+//// by using the '*' as shown below:
+//// [assembly: AssemblyVersion("1.0.*")]
+//[assembly: AssemblyVersion("1.0.0.*")]
+//[assembly: AssemblyFileVersion("1.0.0.0")]
--- src/LiftBridgeUtility6/Properties/AssemblyInfo.cs
+++ src/LiftBridgeUtility6/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
@@ -22,15 +21,16 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("808a3742-65d8-4fc8-ae44-fae4cb47ec6f")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+// The version number now comes from the shared Version.cs file.
+//// Version information for an assembly consists of the following four values:
+////
+////      Major Version
+////      Minor Version
+////      Build Number
+////      Revision
+////
+//// You can specify all the values or you can default the Build and Revision Numbers
+//// by using the '*' as shown below:
+//// [assembly: AssemblyVersion("1.0.*")]
+//[assembly: AssemblyVersion("1.0.0.*")]
+//[assembly: AssemblyFileVersion("1.0.0.0")]
