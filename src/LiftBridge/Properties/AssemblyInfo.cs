af9cc43c6cd78e90d0ac0ce73c05244c1aec9396
Randy Regnier <rbregnier@gmail.com>
2010-11-18 17:50:20 -0600

    Update to FW 6.0.6 dlls and add installer for LiftBridge for FieldWorks 6.

--- src/LiftBridge/Properties/AssemblyInfo.cs
+++ src/LiftBridge/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("LIFTBridge")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("LIFTBridge for FielwdWorks 6")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("SIL")]
 [assembly: AssemblyProduct("LIFTBridge")]
