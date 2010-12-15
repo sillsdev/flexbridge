59821846016402b3e2ea485d36d6c9bae8984699
Randy Regnier <rbregnier@gmail.com>
2010-12-15 15:42:31 -0600

    Work over the two batch files to use relative paths.

--- build/TestInstallerBuild.bat
+++ build/TestInstallerBuild.bat
@@ -1,7 +1,7 @@
 call "c:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
 
-pushd c:\dev\lifttools\build
-MSbuild /target:installer /property:teamcity_build_checkoutDir=c:\dev\lifttools  /property:teamcity_dotnet_nunitlauncher_msbuild_task="notthere" /property:BUILD_NUMBER="*.*.6.789" /property:Minor="1"
+pushd .
+MSbuild /target:installer /property:teamcity_build_checkoutDir=..\  /property:teamcity_dotnet_nunitlauncher_msbuild_task="notthere" /property:BUILD_NUMBER="*.*.6.789" /property:Minor="1"
 popd
 PAUSE
 
