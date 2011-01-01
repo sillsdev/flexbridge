a850f873bbea6ec80dbd47ae64065df279b0ace3
Randy Regnier <rbregnier@gmail.com>
2011-01-01 09:44:53 -0600

    Reduce padding and margin to try and remove white stripes. (Chorus needs to be updated to get rid of it all.)

--- src/FieldWorksBridge/View/ExistingSystemView.designer.cs
+++ src/FieldWorksBridge/View/ExistingSystemView.designer.cs
@@ -43,7 +43,9 @@
 			this._tcMain.Controls.Add(this._tpAbout);
 			this._tcMain.Dock = System.Windows.Forms.DockStyle.Fill;
 			this._tcMain.Location = new System.Drawing.Point(0, 0);
+			this._tcMain.Margin = new System.Windows.Forms.Padding(0);
 			this._tcMain.Name = "_tcMain";
+			this._tcMain.Padding = new System.Drawing.Point(0, 0);
 			this._tcMain.SelectedIndex = 0;
 			this._tcMain.Size = new System.Drawing.Size(450, 309);
 			this._tcMain.TabIndex = 0;
@@ -51,8 +53,8 @@
 			// _tpNotes
 			//
 			this._tpNotes.Location = new System.Drawing.Point(4, 22);
+			this._tpNotes.Margin = new System.Windows.Forms.Padding(0);
 			this._tpNotes.Name = "_tpNotes";
-			this._tpNotes.Padding = new System.Windows.Forms.Padding(3);
 			this._tpNotes.Size = new System.Drawing.Size(442, 283);
 			this._tpNotes.TabIndex = 1;
 			this._tpNotes.Text = "Notes";
@@ -61,8 +63,9 @@
 			// _tpHistory
 			//
 			this._tpHistory.Location = new System.Drawing.Point(4, 22);
+			this._tpHistory.Margin = new System.Windows.Forms.Padding(0);
 			this._tpHistory.Name = "_tpHistory";
-			this._tpHistory.Size = new System.Drawing.Size(573, 335);
+			this._tpHistory.Size = new System.Drawing.Size(442, 283);
 			this._tpHistory.TabIndex = 2;
 			this._tpHistory.Text = "History";
 			this._tpHistory.UseVisualStyleBackColor = true;
@@ -70,8 +73,9 @@
 			// _tpAbout
 			//
 			this._tpAbout.Location = new System.Drawing.Point(4, 22);
+			this._tpAbout.Margin = new System.Windows.Forms.Padding(0);
 			this._tpAbout.Name = "_tpAbout";
-			this._tpAbout.Size = new System.Drawing.Size(573, 335);
+			this._tpAbout.Size = new System.Drawing.Size(442, 283);
 			this._tpAbout.TabIndex = 3;
 			this._tpAbout.Text = "About";
 			this._tpAbout.UseVisualStyleBackColor = true;
@@ -82,6 +86,7 @@
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
 			this.Controls.Add(this._tcMain);
+			this.Margin = new System.Windows.Forms.Padding(0);
 			this.Name = "ExistingSystemView";
 			this.Size = new System.Drawing.Size(450, 309);
 			this._tcMain.ResumeLayout(false);
