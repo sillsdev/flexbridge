060e977929dfab10ae955783d7369c6da4e95a6d
Randy Regnier <rbregnier@gmail.com>
2011-04-10 13:50:56 -0500

    Add some basic sanity checks on the main file pathname argument.

--- src/FieldWorksBridge/Infrastructure/MultipleFileServices.cs
+++ src/FieldWorksBridge/Infrastructure/MultipleFileServices.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 
@@ -9,13 +10,39 @@ namespace FieldWorksBridge.Infrastructure
 	/// Service that will manage the multiple files and original fwdata file for a full FW data set.
 	/// </summary>
 	/// <remarks>
-	/// One task of the service is twofold:
+	/// The task of the service is twofold:
 	/// 1. Break up the main fwdata file into multiple files
 	///		A. one for the custom property declarations, and
-	///		B. one for each concrete CmObject class instances
-	/// 2. Put the multiple files back together into the main fwdata file, if a Send/Receive had new information brought back into the repos.
+	///		B. one for each concrete CmObject class instance
+	/// 2. Put the multiple files back together into the main fwdata file,
+	///		but only if a Send/Receive had new information brought back into the repos.
+	///		NB: The client of the service decides if new information was found, and decides to call the service, or not.
 	/// </remarks>
-	internal class MultipleFileServices
+	internal static class MultipleFileServices
 	{
+		internal static void BreakupMainFile(string mainFilePathname)
+		{
+			CheckPathname(mainFilePathname);
+		}
+
+		internal static void PutHumptyTogetherAgain(string mainFilePathname)
+		{
+			// NB: Do the write on a new file, and then rename (move/copy) to 'mainFilePathname',
+			// just in case it does not finish properly.
+			// NB: This should follow current FW write settings practice.
+			// There is no particular reason to ensure the order of objects in 'mainFilePathname' is retained,
+			// but the custom props element must be first.
+			// Q: Where to find the current model version? That may change in a S/R, and we really want to use the latest model version.
+
+			CheckPathname(mainFilePathname);
+		}
+
+		private static void CheckPathname(string mainFilePathname)
+		{
+			if (string.IsNullOrEmpty(mainFilePathname))
+				throw new ArgumentNullException(mainFilePathname);
+			if (!File.Exists(mainFilePathname))
+				throw new FileNotFoundException("File does not exist.", mainFilePathname);
+		}
 	}
 }
--- src/FieldWorksBridgeTests/Infrastructure/MultipleFileServicesTests.cs
+++ src/FieldWorksBridgeTests/Infrastructure/MultipleFileServicesTests.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using FieldWorksBridge.Infrastructure;
 using NUnit.Framework;
 
 namespace FieldWorksBridgeTests.Infrastructure
@@ -9,5 +11,40 @@ namespace FieldWorksBridgeTests.Infrastructure
 	[TestFixture]
 	public class MultipleFileServicesTests
 	{
+		[Test]
+		public void NullPathnameForBreakupShouldThrow()
+		{
+			Assert.Throws<ArgumentNullException>(() => MultipleFileServices.BreakupMainFile(null));
+		}
+
+		[Test]
+		public void EmptyPathnameForBreakupShouldThrow()
+		{
+			Assert.Throws<ArgumentNullException>(() => MultipleFileServices.BreakupMainFile(""));
+		}
+
+		[Test]
+		public void NonExistingFileForBreakupShouldThrow()
+		{
+			Assert.Throws<FileNotFoundException>(() => MultipleFileServices.BreakupMainFile("Bogus.fwdata"));
+		}
+
+		[Test]
+		public void NullPathnameForRestoreShouldThrow()
+		{
+			Assert.Throws<ArgumentNullException>(() => MultipleFileServices.PutHumptyTogetherAgain(null));
+		}
+
+		[Test]
+		public void EmptyPathnameForRestoreShouldThrow()
+		{
+			Assert.Throws<ArgumentNullException>(() => MultipleFileServices.PutHumptyTogetherAgain(""));
+		}
+
+		[Test]
+		public void NonExistingFileForRestoreShouldThrow()
+		{
+			Assert.Throws<FileNotFoundException>(() => MultipleFileServices.PutHumptyTogetherAgain("Bogus.fwdata"));
+		}
 	}
 }
