e7816c9495639d53dd9b42043f4d16471df4c4d7
Randy Regnier <rbregnier@gmail.com>
2011-08-25 12:07:36 -0500

    Set up build system for Windows vs Linux (later).

--- build/BasicILMerge.bat
+++
@@ -1,2 +0,0 @@
-ILMerge /lib:..\lib\net2.0\ /lib:..\lib\common\ /target:library /out:..\output\common\ChorusPlus.dll Chorus.exe LibChorus.dll Palaso.dll icu.net.dll
-ILMerge /lib:..\lib\net2.0\ /lib:..\lib\common\ /out:..\output\common\ChorusMerge.exe ChorusMerge.exe LibChorus.dll Palaso.dll icu.net.dll
