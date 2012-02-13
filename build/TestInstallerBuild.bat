2a9895c33a591d49d1b2d1bf4b509c006fa0dea9
Randy Regnier <rbregnier@gmail.com>
2012-02-13 14:05:15 -0600

    Mess with installer some more (no joy in upgrade-land).

--- build/TestInstallerBuild.bat
+++ build/TestInstallerBuild.bat
@@ -1,6 +1,6 @@
 call "c:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
 
 pushd .
-MSbuild LiftBridge.build.win.proj /target:Installer /property:teamcity_build_checkoutDir=..\  /property:teamcity_dotnet_nunitlauncher_msbuild_task="notthere" /property:BUILD_NUMBER="*.*.*.1" /property:Minor="1"
+MSbuild LiftBridge.build.win.proj /target:Installer /property:teamcity_build_checkoutDir=..\  /property:teamcity_dotnet_nunitlauncher_msbuild_task="notthere" /property:BUILD_NUMBER="*.*.2.*" /property:Minor="1"
 popd
 PAUSE
