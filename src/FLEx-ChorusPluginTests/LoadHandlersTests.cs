9e18701c68a981db785ac3b0264d9f686d9e379c
Randy Regnier <rbregnier@gmail.com>
2012-03-07 22:31:33 -0600

    Refactor returning extensions.

--- src/FLEx-ChorusPluginTests/LoadHandlersTests.cs
+++ src/FLEx-ChorusPluginTests/LoadHandlersTests.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using Chorus.FileTypeHanders;
+using FLEx_ChorusPlugin.Infrastructure;
 using NUnit.Framework;
 
 namespace FLEx_ChorusPluginTests
@@ -23,6 +24,52 @@ namespace FLEx_ChorusPluginTests
 											};
 			foreach (var unexpectedBridgeHandler in unexpectedBridgeHandlers)
 				Assert.IsFalse(handlerNames.Contains(unexpectedBridgeHandler));
+		}
+
+		[Test]
+		public void EnsureAllSupportedExtensionsAreReturned()
+		{
+
+			var supportedExtensions = new HashSet<string>
+			{
+				// Common
+				SharedConstants.ModelVersion,		// Better validation done.
+				SharedConstants.CustomProperties,	// Better validation done.
+				SharedConstants.Style,
+				SharedConstants.List,
+
+				// General
+				SharedConstants.lint,
+				SharedConstants.langproj,
+				SharedConstants.Annotation,
+				SharedConstants.Filter,
+
+				// Scripture
+				SharedConstants.ArchivedDraft,
+				SharedConstants.ImportSetting,
+				SharedConstants.Srs,
+				SharedConstants.Trans,
+
+				// Anthropology
+				SharedConstants.Ntbk,
+
+				// Linguistics
+				SharedConstants.Reversal,
+				SharedConstants.Lexdb, // The lexicon only added one new extension "lexdb", as the lists are already taken care of.
+				SharedConstants.TextInCorpus, // Text corpus only added one new extension "textincorpus", as the list is already taken care of.
+				SharedConstants.Inventory, // inventory
+				SharedConstants.DiscourseExt, // discourse
+				SharedConstants.Featsys, // Feature structure systems (Phon and Morph & Syn)
+				SharedConstants.Phondata,
+				SharedConstants.Morphdata,
+				SharedConstants.Agents
+			};
+
+			var commonHandler = (from handler in ChorusFileTypeHandlerCollection.CreateWithInstalledHandlers().Handlers
+						   where handler.GetType().Name == "FieldWorksCommonFileHandler"
+						   select handler).First();
+			var knownExtensions = new HashSet<string>(commonHandler.GetExtensionsOfKnownTextFileTypes());
+			Assert.IsTrue(knownExtensions.SetEquals(supportedExtensions));
 		}
 	}
 }
