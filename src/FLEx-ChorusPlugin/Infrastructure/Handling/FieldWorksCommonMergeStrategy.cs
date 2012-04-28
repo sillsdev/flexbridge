1ec0bacaa1f7e7bc363f53b5b5938f522640882a
Randy Regnier <rbregnier@gmail.com>
2012-04-28 13:21:02 -0500

    Update FLEx Bridge code in anticipation of CHR-13 being merged into Chorus default.

--- src/FLEx-ChorusPlugin/Infrastructure/Handling/FieldWorksCommonMergeStrategy.cs
+++ src/FLEx-ChorusPlugin/Infrastructure/Handling/FieldWorksCommonMergeStrategy.cs
@@ -29,11 +29,24 @@ namespace FLEx_ChorusPlugin.Infrastructure.Handling
 
 		#region Implementation of IMergeStrategy
 
+		/// <summary>
+		/// Produce a string that represents the 3-way merger of the given three elements.
+		/// </summary>
 		public string MakeMergedEntry(IMergeEventListener eventListener, XmlNode ourEntry, XmlNode theirEntry, XmlNode commonEntry)
 		{
 			FieldWorksMergingServices.PreMerge(_mdc, ourEntry, theirEntry, commonEntry);
 
 			return _merger.Merge(eventListener, ourEntry, theirEntry, commonEntry).OuterXml;
+		}
+
+		/// <summary>
+		/// Return the ElementStrategy instance for the given <param name="element"/>, or a default instance set up like this:
+		/// ElementStrategy def = new ElementStrategy(true);//review: this says the default is to consder order relevant
+		/// def.MergePartnerFinder = new FindByEqualityOfTree();
+		/// </summary>
+		public ElementStrategy GetElementStrategy(XmlNode element)
+		{
+			return _merger.MergeStrategies.GetElementStrategy(element);
 		}
 
 		#endregion
