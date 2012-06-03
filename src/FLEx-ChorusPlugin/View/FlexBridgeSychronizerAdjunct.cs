a0c53fdf58dbadd949aff1e60aef55f7d400fddf
Randy Regnier <rbregnier@gmail.com>
2012-06-03 11:51:39 -0500

    Add implementation of Chorus' ISychronizerAdjunct interface.

---
+++ src/FLEx-ChorusPlugin/View/FlexBridgeSychronizerAdjunct.cs
@@ -0,0 +1,41 @@
+using System.IO;
+using System.Windows.Forms;
+using Chorus.sync;
+using FLEx_ChorusPlugin.Infrastructure.DomainServices;
+using Palaso.Progress.LogBox;
+
+namespace FLEx_ChorusPlugin.View
+{
+	internal sealed class FlexBridgeSychronizerAdjunct : ISychronizerAdjunct
+	{
+		private readonly Form _parent;
+		private readonly string _fwdataPathname;
+
+		internal FlexBridgeSychronizerAdjunct(Form parent, string fwdataPathname)
+		{
+			_parent = parent;
+			_fwdataPathname = fwdataPathname;
+		}
+
+		private string ProjectFilename
+		{
+			get { return Path.GetFileName(_fwdataPathname); }
+		}
+
+		#region Implementation of ISychronizerAdjunct
+
+		public void PrepareForInitialCommit(IProgress progress)
+		{
+			progress.WriteMessage("Split up project file: {0}", ProjectFilename);
+			FLExProjectSplitter.SplitFwdataDelegate(_parent, _fwdataPathname);
+		}
+
+		public void PrepareForPostMergeCommit(IProgress progress, int totalNumberOfMerges, int currentMerge)
+		{
+			progress.WriteMessage("Restore project file '{0}' for merge {1} of {2}", ProjectFilename, currentMerge, totalNumberOfMerges);
+			FLExProjectUnifier.UnifyFwdataProgress(_parent, _fwdataPathname);
+		}
+
+		#endregion
+	}
+}
