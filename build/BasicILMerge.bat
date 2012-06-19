5b18be0e81578ac0e0d6899c715733a9eb8ae312
Randy Regnier <rbregnier@gmail.com>
2012-06-19 14:13:39 -0500

    Get build working again.

--- build/BasicILMerge.bat
+++ build/BasicILMerge.bat
@@ -1,2 +1,5 @@
+mkdir ..\output
+mkdir ..\output\common
+
 ILMerge /lib:..\lib\Release\ /lib:..\lib\common\ /target:library /out:..\output\common\ChorusPlus.dll Chorus.exe LibChorus.dll Autofac.dll Palaso.dll PalasoUIWindowsForms.dll icu.net.dll Enchant.Net.dll Interop.WIA.dll Keyman7Interop.dll KeymanLink.dll
 ILMerge /lib:..\lib\Release\ /lib:..\lib\common\ /out:..\output\common\ChorusMerge.exe ChorusMerge.exe LibChorus.dll Autofac.dll Palaso.dll PalasoUIWindowsForms.dll icu.net.dll Enchant.Net.dll Interop.WIA.dll Keyman7Interop.dll KeymanLink.dll
