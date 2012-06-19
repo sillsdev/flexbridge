10f628baa2331858f9df432bf4961455527fc9e3
Randy Regnier <rbregnier@gmail.com>
2012-06-19 15:47:53 -0500

    Only use one merged assembly now (ChorusMerge.exe).

--- build/BasicILMerge.bat
+++ build/BasicILMerge.bat
@@ -1,5 +1,1 @@
-mkdir ..\output
-mkdir ..\output\common
-
-ILMerge /lib:..\lib\Release\ /lib:..\lib\common\ /target:library /out:..\output\common\ChorusPlus.dll Chorus.exe LibChorus.dll Autofac.dll Palaso.dll PalasoUIWindowsForms.dll icu.net.dll Enchant.Net.dll Interop.WIA.dll Keyman7Interop.dll KeymanLink.dll
-ILMerge /lib:..\lib\Release\ /lib:..\lib\common\ /out:..\output\common\ChorusMerge.exe ChorusMerge.exe LibChorus.dll Autofac.dll Palaso.dll PalasoUIWindowsForms.dll icu.net.dll Enchant.Net.dll Interop.WIA.dll Keyman7Interop.dll KeymanLink.dll
+ILMerge /lib:..\lib\Release\ /lib:..\lib\common\ /out:..\output\common\ChorusMerge.exe ChorusMerge.exe LibChorus.dll Chorus.exe Autofac.dll Palaso.dll PalasoUIWindowsForms.dll icu.net.dll Enchant.Net.dll Interop.WIA.dll Keyman7Interop.dll KeymanLink.dll
