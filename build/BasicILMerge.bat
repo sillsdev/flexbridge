fad5a7b4853874237c782ec6d3857ee861b6a5a9
Randy Regnier <rbregnier@gmail.com>
2012-06-19 13:46:19 -0500

    Round 1 of restoring ILMerge.

---
+++ build/BasicILMerge.bat
@@ -0,0 +1,2 @@
+ILMerge /lib:..\lib\Release\ /lib:..\lib\common\ /target:library /out:..\output\common\ChorusPlus.dll Chorus.exe LibChorus.dll Autofac.dll Palaso.dll PalasoUIWindowsForms.dll icu.net.dll Enchant.Net.dll Interop.WIA.dll Keyman7Interop.dll KeymanLink.dll
+ILMerge /lib:..\lib\Release\ /lib:..\lib\common\ /out:..\output\common\ChorusMerge.exe ChorusMerge.exe LibChorus.dll Autofac.dll Palaso.dll PalasoUIWindowsForms.dll icu.net.dll Enchant.Net.dll Interop.WIA.dll Keyman7Interop.dll KeymanLink.dll
