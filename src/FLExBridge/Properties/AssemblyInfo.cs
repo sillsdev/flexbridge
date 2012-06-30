986571cf04c59ba1648a9cb707fd6b4624550a50
Randy Regnier <rbregnier@gmail.com>
2012-06-30 18:02:07 -0500

    Update assembly info.

--- src/FLExBridge/Properties/AssemblyInfo.cs
+++ src/FLExBridge/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+﻿	using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("FLExBridge")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("SIL")]
 [assembly: AssemblyProduct("FLExBridge")]
-[assembly: AssemblyCopyright("Copyright ©  2010")]
+[assembly: AssemblyCopyright("Copyright © SIL 2010-2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.*")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.3.55.*")]
+[assembly: AssemblyFileVersion("0.3.55.0")]
 
 [assembly: InternalsVisibleTo("FieldWorksBridgeTests")]
