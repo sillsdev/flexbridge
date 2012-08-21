d3bf473160554c37141ad2e7a73f750476515081
Jason Naylor <jason.naylor@gmail.com>
2012-08-21 18:07:47 -0500

    First proof of concept for using LocalizationManager. Still need to bring in the LocalizationManager.dll from somewhere real, and figure out what to do with strings if we can't localize them when they come from Resources.

--- src/FLEx-ChorusPlugin/View/StartupNewView.designer.cs
+++ src/FLEx-ChorusPlugin/View/StartupNewView.designer.cs
@@ -1,4 +1,5 @@
 ﻿using Chorus.UI.Clone;
+using Localization.UI;
 
 namespace FLEx_ChorusPlugin.View
 {
@@ -6,66 +7,69 @@ namespace FLEx_ChorusPlugin.View
 	{
 		private void InitializeComponent()
 		{
+			this.components = new System.ComponentModel.Container();
 			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(StartupNewView));
 			this._useUSBButton = new System.Windows.Forms.Button();
 			this._useInternetButton = new System.Windows.Forms.Button();
 			this._useSharedFolderButton = new System.Windows.Forms.Button();
+			this.localizer = new Localization.UI.LocalizationExtender(this.components);
+			((System.ComponentModel.ISupportInitialize)(this.localizer)).BeginInit();
 			this.SuspendLayout();
 			//
 			// _useUSBButton
 			//
 			this._useUSBButton.BackColor = System.Drawing.Color.White;
-			this._useUSBButton.Image = ((System.Drawing.Image)(resources.GetObject("_useUSBButton.Image")));
-			this._useUSBButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-			this._useUSBButton.Enabled = new CloneFromUsb().GetHaveOneOrMoreUsbDrives();
-			this._useUSBButton.Location = new System.Drawing.Point(17, 45);
+			resources.ApplyResources(this._useUSBButton, "_useUSBButton");
+			this.localizer.SetLocalizableToolTip(this._useUSBButton, "USB button tooltip");
+			this.localizer.SetLocalizationComment(this._useUSBButton, "Text for USB button on obtain dialog");
+			this.localizer.SetLocalizingId(this._useUSBButton, "StartupNewView._useUSBButton");
 			this._useUSBButton.Name = "_useUSBButton";
-			this._useUSBButton.Size = new System.Drawing.Size(167, 39);
-			this._useUSBButton.TabIndex = 3;
-			this._useUSBButton.Text = "&USB Flash Drive";
-			this._useUSBButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
 			this._useUSBButton.UseVisualStyleBackColor = false;
 			this._useUSBButton.Click += new System.EventHandler(this._useUSBButton_Click);
 			//
 			// _useInternetButton
 			//
 			this._useInternetButton.BackColor = System.Drawing.Color.White;
-			this._useInternetButton.Image = ((System.Drawing.Image)(resources.GetObject("_useInternetButton.Image")));
-			this._useInternetButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-			this._useInternetButton.Location = new System.Drawing.Point(17, 120);
-			this._useInternetButton.Enabled = System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable();
+			resources.ApplyResources(this._useInternetButton, "_useInternetButton");
+			this.localizer.SetLocalizableToolTip(this._useInternetButton, "Obtain from the internet");
+			this.localizer.SetLocalizationComment(this._useInternetButton, "Button text for using the internet similar to engilsh for \'Internet\'");
+			this.localizer.SetLocalizingId(this._useInternetButton, "StartupNewView._useInternetButton");
 			this._useInternetButton.Name = "_useInternetButton";
-			this._useInternetButton.Size = new System.Drawing.Size(167, 39);
-			this._useInternetButton.TabIndex = 2;
-			this._useInternetButton.Text = "&Internet";
-			this._useInternetButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
 			this._useInternetButton.UseVisualStyleBackColor = false;
 			this._useInternetButton.Click += new System.EventHandler(this._useInternetButton_Click);
 			//
 			// _useSharedFolderButton
 			//
 			this._useSharedFolderButton.BackColor = System.Drawing.Color.White;
-			this._useSharedFolderButton.Enabled = System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable();
-			this._useSharedFolderButton.Image = ((System.Drawing.Image)(resources.GetObject("_useSharedFolderButton.Image")));
-			this._useSharedFolderButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-			this._useSharedFolderButton.Location = new System.Drawing.Point(17, 199);
+			resources.ApplyResources(this._useSharedFolderButton, "_useSharedFolderButton");
+			this.localizer.SetLocalizableToolTip(this._useSharedFolderButton, "For user settings");
+			this.localizer.SetLocalizationComment(this._useSharedFolderButton, "Button text meaning \"Shared Network Folder\"");
+			this.localizer.SetLocalizingId(this._useSharedFolderButton, "StartupNewView._useSharedFolderButton");
 			this._useSharedFolderButton.Name = "_useSharedFolderButton";
-			this._useSharedFolderButton.Size = new System.Drawing.Size(167, 39);
-			this._useSharedFolderButton.TabIndex = 1;
-			this._useSharedFolderButton.Text = "&Shared Network Folder";
-			this._useSharedFolderButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
 			this._useSharedFolderButton.UseVisualStyleBackColor = false;
 			this._useSharedFolderButton.Click += new System.EventHandler(this._useSharedFolderButton_Click);
+			//
+			// localizer
+			//
+			this.localizer.LocalizationManagerId = "FLExBridge";
 			//
 			// StartupNewView
 			//
 			this.Controls.Add(this._useUSBButton);
 			this.Controls.Add(this._useInternetButton);
 			this.Controls.Add(this._useSharedFolderButton);
+			this.localizer.SetLocalizableToolTip(this, null);
+			this.localizer.SetLocalizationComment(this, null);
+			this.localizer.SetLocalizingId(this, "StartupNewView.StartupNewView");
 			this.Name = "StartupNewView";
-			this.Size = new System.Drawing.Size(201, 247);
+			resources.ApplyResources(this, "$this");
+			((System.ComponentModel.ISupportInitialize)(this.localizer)).EndInit();
 			this.ResumeLayout(false);
+
 		}
+
+		private Localization.UI.LocalizationExtender localizer;
+		private System.ComponentModel.IContainer components;
 
 	}
 }
