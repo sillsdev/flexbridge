4b4663653e1f5af7f0c91201b8fea78ac2f9f073
Randy Regnier <rbregnier@gmail.com>
2012-11-10 17:20:09 -0600

    Fix assembly names and output folders

--- src/LiftBridge-ChorusPlugin/Properties/AssemblyInfo.cs
+++ src/LiftBridge-ChorusPlugin/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LIFTBridge")]
+[assembly: AssemblyTitle("LiftBridge-ChorusPlugin")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("SIL")]
-[assembly: AssemblyProduct("LIFTBridge")]
+[assembly: AssemblyProduct("LiftBridge-ChorusPlugin")]
 [assembly: AssemblyCopyright("Copyright © 2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -33,8 +33,8 @@ using System.Runtime.InteropServices;
 //// You can specify all the values or you can default the Build and Revision Numbers
 //// by using the '*' as shown below:
 //// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.*")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.*")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
 
-[assembly: InternalsVisibleTo("LiftBridgeTests")]
+[assembly: InternalsVisibleTo("LiftBridge-ChorusPluginTests")]
 [assembly: InternalsVisibleTo("LiftBridge")]
