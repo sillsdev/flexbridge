3636c38ae76e8167d2a6e32768575d8b9c3e96b3
Randy Regnier <rbregnier@gmail.com>
2013-05-04 10:33:11 -0500

    Switch to use common assembly info file plus regular AssemblyInfo file for each project

--- src/FLEx-ChorusPlugin/Properties/AssemblyInfo.cs
+++ src/FLEx-ChorusPlugin/Properties/AssemblyInfo.cs
@@ -2,39 +2,8 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("FLEx-ChorusPlugin")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("SIL")]
-[assembly: AssemblyProduct("FLEx-ChorusPlugin")]
-[assembly: AssemblyCopyright("Copyright © SIL 2011-2012")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("52c8c11d-6c44-49c0-bff0-cdfbbfde54f5")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.5.2.*")]
-[assembly: AssemblyFileVersion("0.5.2.0")]
-
 [assembly: InternalsVisibleTo("FLExBridge")]
 [assembly: InternalsVisibleTo("FLEx-ChorusPluginTests")]
 [assembly: InternalsVisibleTo("FwdataTestApp")]
--- src/TriboroughBridge-ChorusPlugin/Properties/AssemblyInfo.cs
+++ src/TriboroughBridge-ChorusPlugin/Properties/AssemblyInfo.cs
@@ -2,39 +2,8 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("TriboroughBridge-ChorusPlugin")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("SIL")]
-[assembly: AssemblyProduct("TriboroughBridge-ChorusPlugin")]
-[assembly: AssemblyCopyright("Copyright © SIL 2012")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("fa7b5961-289b-4ff6-bb8a-2ee8c9c59e66")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.5.2.*")]
-[assembly: AssemblyFileVersion("0.5.2.0")]
-
 [assembly: InternalsVisibleTo("TriboroughBridge-ChorusPluginTests")]
 [assembly: InternalsVisibleTo("FLEx-ChorusPluginTests")]
 [assembly: InternalsVisibleTo("LiftBridge-ChorusPluginTests")]
