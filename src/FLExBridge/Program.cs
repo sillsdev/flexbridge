a4dffec964a9bcca804045068be07e692d859439
Randy Regnier <rbregnier@gmail.com>
2013-06-26 16:19:02 -0500

    Add '-locale' to command line validator and move the L10 code out of main app

--- src/FLExBridge/Program.cs
+++ src/FLExBridge/Program.cs
@@ -1,13 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.Composition.Hosting;
 using System.IO;
-using System.Reflection;
 using System.Windows.Forms;
-using Chorus;
 using Chorus.VcsDrivers.Mercurial;
 using FLEx_ChorusPlugin.Properties;
-using L10NSharp;
 using Palaso.Reporting;
 using Palaso.UI.WindowsForms.HotSpot;
 using TriboroughBridge_ChorusPlugin;
@@ -19,10 +15,6 @@ namespace FLExBridge
 {
 	static class Program
 	{
-		private const string FlexBridge = "FlexBridge";
-		private const string localizations = "localizations";
-		private const string FlexBridgeEmailAddress = "fieldworksbridge@gmail.com";
-
 		/// <summary>
 		/// The main entry point for the application.
 		/// </summary>
@@ -57,8 +49,7 @@ namespace FLExBridge
 			}
 
 			var options = CommandLineProcessor.ParseCommandLineArgs(args);
-
-			var l10Managers = SetupLocalization(options);
+			var l10Managers = Utilities.SetupLocalization(options);
 
 			// An aggregate catalog that combines multiple catalogs
 			using (var catalog = new AggregateCatalog())
@@ -107,56 +98,9 @@ namespace FLExBridge
 			Settings.Default.Save();
 		}
 
-		private static Dictionary<string, LocalizationManager> SetupLocalization(Dictionary<string, string> options)
-		{
-			var results = new Dictionary<string, LocalizationManager>(3);
-
-			string desiredUiLangId;
-			if (!options.TryGetValue("-locale", out desiredUiLangId))
-				desiredUiLangId = "en";
-			var installedTmxBaseDirectory = Path.Combine(Path.GetDirectoryName(Utilities.StripFilePrefix(Assembly.GetExecutingAssembly().CodeBase)), localizations);
-			var userTmxBaseDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), "SIL", FlexBridge, localizations);
-
-			// Now set it up for the handful of localizable elements in FlexBridge itself.
-			// This is safer than Application.ProductVersion, which might contain words like 'alpha' or 'beta',
-			// which (on the SECOND run of the program) fail when L10NSharp tries to make a Version object out of them.
-			var versionObj = Assembly.GetExecutingAssembly().GetName().Version;
-			// We don't need to reload strings for every "revision" (that might be every time we build).
-			var version = "" + versionObj.Major + "." + versionObj.Minor + "." + versionObj.Build;
-			var flexBridgeLocMan = LocalizationManager.Create(desiredUiLangId, FlexBridge, Application.ProductName,
-				version,
-				installedTmxBaseDirectory,
-				userTmxBaseDirectory,
-				Resources.chorus,
-				FlexBridgeEmailAddress, new string[] { FlexBridge, "TriboroughBridge_ChorusPlugin", "FLEx_ChorusPlugin", "SIL.LiftBridge" });
-			results.Add("FlexBridge", flexBridgeLocMan);
-
-			versionObj = Assembly.GetAssembly(typeof(ChorusSystem)).GetName().Version;
-			version = "" + versionObj.Major + "." + versionObj.Minor + "." + versionObj.Build;
-			var chorusLocMan = LocalizationManager.Create(desiredUiLangId, "Chorus", "Chorus",
-				version,
-				installedTmxBaseDirectory,
-				userTmxBaseDirectory,
-				Resources.chorus,
-				FlexBridgeEmailAddress, "Chorus");
-			results.Add("Chorus", chorusLocMan);
-
-			versionObj = Assembly.GetAssembly(typeof(ErrorReport)).GetName().Version;
-			version = "" + versionObj.Major + "." + versionObj.Minor + "." + versionObj.Build;
-			var palasoLocMan = LocalizationManager.Create(desiredUiLangId, "Palaso", "Palaso",
-				version,
-				installedTmxBaseDirectory,
-				userTmxBaseDirectory,
-				Resources.chorus,
-				FlexBridgeEmailAddress, "Palaso");
-			results.Add("Palaso", palasoLocMan);
-
-			return results;
-		}
-
 		private static void SetUpErrorHandling()
 		{
-			ErrorReport.EmailAddress = FlexBridgeEmailAddress;
+			ErrorReport.EmailAddress = Utilities.FlexBridgeEmailAddress;
 			ErrorReport.AddStandardProperties();
 			ExceptionHandler.Init();
 		}
