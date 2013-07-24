cb0e10ec9b99b2fe9f8ea49e046f8eb44597ead5
Hasso Pape <hasso_pape@sil.org>
2013-07-24 13:30:02 -0500

    Add script to update build dependencies

---
+++ UpdateDependencies.bat
@@ -0,0 +1,16 @@
+copy /Y ..\chorus\output\debug\L10NSharp.dll lib\common\
+
+copy /Y ..\chorus\output\debug\LibChorus.dll lib\debug\
+copy /Y ..\chorus\output\debug\LibChorus.pdb lib\debug\
+
+copy /Y ..\chorus\output\debug\Chorus.exe lib\debug\
+copy /Y ..\chorus\output\debug\Chorus.pdb lib\debug\
+
+copy /Y ..\chorus\output\debug\ChorusMerge.exe lib\debug\
+copy /Y ..\chorus\output\debug\ChorusMerge.pdb lib\debug\
+
+copy /Y ..\chorus\output\debug\ChorusHub.exe lib\debug\
+copy /Y ..\chorus\output\debug\ChorusHub.pdb lib\debug\
+
+copy /Y ..\chorus\output\debug\Palaso*.dll lib\debug\
+copy /Y ..\chorus\output\debug\Palaso*.pdb lib\debug\
