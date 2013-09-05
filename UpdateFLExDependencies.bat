db04d0e5f8a2e49a17b08c07c14c235d70f4e38b
Hasso Pape <hasso_pape@sil.org>
2013-09-05 16:59:21 -0500

    Update UpdateDependencies script; add a script to update FLEx's dependencies

---
+++ UpdateFLExDependencies.bat
@@ -0,0 +1,14 @@
+copy /Y ..\chorus\output\debug\LibChorus.dll \fwrepo\fw\Output\Debug\
+copy /Y ..\chorus\output\debug\LibChorus.pdb \fwrepo\fw\Output\Debug\
+
+copy /Y ..\chorus\output\debug\Chorus.exe \fwrepo\fw\Output\Debug\
+copy /Y ..\chorus\output\debug\Chorus.pdb \fwrepo\fw\Output\Debug\
+
+copy /Y ..\chorus\Download\IPCFramework.dll \fwrepo\fw\Output\Debug\
+
+copy /Y ..\chorus\output\debug\L10NSharp.dll \fwrepo\fw\Output\Debug\
+REM Uncomment this line if you are working on L10NSharp
+REM copy /Y ..\l10nsharp\output\Debug\L10NSharp.* \fwrepo\fw\Output\Debug\
+
+copy /Y ..\chorus\output\debug\Palaso*.dll \fwrepo\fw\Output\Debug\
+copy /Y ..\chorus\output\debug\Palaso*.pdb \fwrepo\fw\Output\Debug\
