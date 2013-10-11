c1e6795e2ed07dcc115d4b26aed7e1eab40554a3
Randy Regnier <rbregnier@gmail.com>
2013-10-11 11:38:30 -0700

    Update GetAndBuildThis and UpdateDependencies batch files

--- UpdateDependencies.bat
+++ UpdateDependencies.bat
@@ -1,33 +1,40 @@
 REM Since Chorus and Palaso libraries change frequently, you will likely need to get those
 REM projects and be able to build them.  Run this script to build and copy those libraries.
 REM This script assumes that the Chorus and Palaso directories are on the same level as
-REM this one, and that the FieldWorks repo is in C:\fwrepo.
+REM this one, and that the FieldWorks repo is also at the same level.
 REM It copies the needed libraries into the lib folder.
 
-pushd ..\chorus
-echo y | call GetAndBuildThis.bat
+IF "%1"=="" (
+	set BUILD_CONFIG="Debug"
+) ELSE (
+	set BUILD_CONFIG=%1
+)
+
+pushd .
+cd ..\chorus
+echo y | call GetAndBuildThis.bat %BUILD_CONFIG% %2
 popd
 
 REM FieldWorks build downloads this but we don't (TODO pH 2013.09: fix our build script)
-copy /Y \fwrepo\fw\Downloads\IPCFramework.dll lib\Debug\
+copy /Y ..\fwrepo\fw\Downloads\IPCFramework.dll lib\%BUILD_CONFIG%\
 
 copy /Y ..\chorus\Download\L10NSharp.dll lib\common\
 REM Uncomment these two lines if you are working on L10NSharp
-REM copy /Y ..\l10nsharp\output\Debug\L10NSharp.dll lib\common\
-REM copy /Y ..\l10nsharp\output\Debug\L10NSharp.* lib\Debug\
+REM copy /Y ..\l10nsharp\output\%BUILD_CONFIG%\L10NSharp.dll lib\common\
+REM copy /Y ..\l10nsharp\output\%BUILD_CONFIG%\L10NSharp.* lib\%BUILD_CONFIG%\
 
-copy /Y ..\chorus\output\Debug\LibChorus.TestUtilities.dll lib\Debug\
-copy /Y ..\chorus\output\Debug\LibChorus.dll lib\Debug\
-copy /Y ..\chorus\output\Debug\LibChorus.pdb lib\Debug\
+copy /Y ..\chorus\output\%BUILD_CONFIG%\LibChorus.TestUtilities.dll lib\%BUILD_CONFIG%\
+copy /Y ..\chorus\output\%BUILD_CONFIG%\LibChorus.dll lib\%BUILD_CONFIG%\
+copy /Y ..\chorus\output\%BUILD_CONFIG%\LibChorus.pdb lib\%BUILD_CONFIG%\
 
-copy /Y ..\chorus\output\Debug\Chorus.exe lib\Debug\
-copy /Y ..\chorus\output\Debug\Chorus.pdb lib\Debug\
+copy /Y ..\chorus\output\%BUILD_CONFIG%\Chorus.exe lib\%BUILD_CONFIG%\
+copy /Y ..\chorus\output\%BUILD_CONFIG%\Chorus.pdb lib\%BUILD_CONFIG%\
 
-copy /Y ..\chorus\output\Debug\ChorusMerge.exe lib\Debug\
-copy /Y ..\chorus\output\Debug\ChorusMerge.pdb lib\Debug\
+copy /Y ..\chorus\output\%BUILD_CONFIG%\ChorusMerge.exe lib\%BUILD_CONFIG%\
+copy /Y ..\chorus\output\%BUILD_CONFIG%\ChorusMerge.pdb lib\%BUILD_CONFIG%\
 
-copy /Y ..\chorus\output\Debug\ChorusHub.exe lib\Debug\
-copy /Y ..\chorus\output\Debug\ChorusHub.pdb lib\Debug\
+copy /Y ..\chorus\output\%BUILD_CONFIG%\ChorusHub.exe lib\%BUILD_CONFIG%\
+copy /Y ..\chorus\output\%BUILD_CONFIG%\ChorusHub.pdb lib\%BUILD_CONFIG%\
 
-copy /Y ..\chorus\output\Debug\Palaso*.dll lib\Debug\
-copy /Y ..\chorus\output\Debug\Palaso*.pdb lib\Debug\
+copy /Y ..\chorus\output\%BUILD_CONFIG%\Palaso*.dll lib\%BUILD_CONFIG%\
+copy /Y ..\chorus\output\%BUILD_CONFIG%\Palaso*.pdb lib\%BUILD_CONFIG%\
