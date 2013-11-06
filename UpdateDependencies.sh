106b6552d0bd25e291c205416b539b6499a918db
Hasso Pape <hasso_pape@sil.org>
2013-11-06 15:22:01 -0600

    Update UpdateDependencies

--- UpdateDependencies.sh
+++ UpdateDependencies.sh
@@ -4,24 +4,32 @@
 # this one, and that the FieldWorks repo is in C:/fwrepo.
 # It copies the needed libraries into the lib folder.
 
+if [ "$1"=="" ]
+then
+		BUILD_CONFIG=Debug
+else
+		BUILD_CONFIG=$1
+fi
+
 cp ../chorus/Download/L10NSharp.dll* lib/common/
 # Uncomment these two lines if you are working on L10NSharp
-# cp ../l10nsharp/output/DebugMono/L10NSharp.dll lib/common/
-# cp ../l10nsharp/output/DebugMono/L10NSharp.* lib/DebugMono/
+# cp ../l10nsharp/output/${BUILD_CONFIG}Mono/L10NSharp.dll lib/common/
+# cp ../l10nsharp/output/${BUILD_CONFIG}Mono/L10NSharp.* lib/${BUILD_CONFIG}Mono/
 
-cp ../chorus/output/DebugMono/LibChorus.TestUtilities.dll* lib/DebugMono/
-cp ../chorus/output/DebugMono/LibChorus.TestUtilities.dll* output/DebugMono/
-cp ../chorus/output/DebugMono/LibChorus.dll* lib/DebugMono/
-cp ../chorus/output/DebugMono/LibChorus.dll* output/DebugMono/
+cp ../chorus/output/${BUILD_CONFIG}Mono/LibChorus.TestUtilities.dll* lib/${BUILD_CONFIG}Mono/
+cp ../chorus/output/${BUILD_CONFIG}Mono/LibChorus.TestUtilities.dll* output/${BUILD_CONFIG}Mono/
+cp ../chorus/output/${BUILD_CONFIG}Mono/LibChorus.dll* lib/${BUILD_CONFIG}Mono/
+cp ../chorus/output/${BUILD_CONFIG}Mono/LibChorus.dll* output/${BUILD_CONFIG}Mono/
 
-cp ../chorus/output/DebugMono/Chorus.exe* lib/DebugMono/
-cp ../chorus/output/DebugMono/Chorus.exe* output/DebugMono/
+cp ../chorus/output/${BUILD_CONFIG}Mono/Chorus.exe* lib/${BUILD_CONFIG}Mono/
+cp ../chorus/output/${BUILD_CONFIG}Mono/Chorus.exe* output/${BUILD_CONFIG}Mono/
 
-cp ../chorus/output/DebugMono/ChorusMerge.exe* lib/DebugMono/
-cp ../chorus/output/DebugMono/ChorusMerge.exe* output/DebugMono/
+cp ../chorus/output/${BUILD_CONFIG}Mono/ChorusMerge.exe* lib/${BUILD_CONFIG}Mono/
+cp ../chorus/output/${BUILD_CONFIG}Mono/ChorusMerge.exe* output/${BUILD_CONFIG}Mono/
 
-cp ../chorus/output/DebugMono/ChorusHub.exe* lib/DebugMono/
-cp ../chorus/output/DebugMono/ChorusHub.exe* output/DebugMono/
+cp ../chorus/output/${BUILD_CONFIG}Mono/ChorusHub.exe* lib/${BUILD_CONFIG}Mono/
+cp ../chorus/output/${BUILD_CONFIG}Mono/ChorusHub.exe* output/${BUILD_CONFIG}Mono/
 
-cp ../chorus/output/DebugMono/Palaso*.dll* lib/DebugMono/
-cp ../chorus/output/DebugMono/Palaso*.dll* output/DebugMono/
+cp ../chorus/output/${BUILD_CONFIG}Mono/Palaso*.dll* lib/${BUILD_CONFIG}Mono/
+cp ../chorus/output/${BUILD_CONFIG}Mono/Palaso*.dll* output/${BUILD_CONFIG}Mono/
+
