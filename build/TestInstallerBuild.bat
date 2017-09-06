9b3592323513a588aed008594a650415677e7e20
vkarthim <vkarthim@gmail.com>
2017-09-06 20:58:37 +0530

    LT-16680:Send Receive menu unavailable while FLExBridge installed (#153)
    * LT-16680:Send Receive menu unavailable while FLExBridge installed
    
    *Updated the setting for FLExBridge
    *Changed the FLExBridge version number to 9 for registry
    *Removed unused Palaso related files.
    *Included SIL related files.
    *Updated the version number to 9. LT-18560

--- build/TestInstallerBuild.bat
+++ build/TestInstallerBuild.bat
@@ -1,6 +1,6 @@
-call "c:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
+call "c:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"
 
 pushd .
-MSbuild FLExBridge.build.win.proj /target:Installer /property:teamcity_build_checkoutDir=..\  /property:teamcity_dotnet_nunitlauncher_msbuild_task="notthere" /property:BUILD_NUMBER="0.3.1" /property:Minor="1"
+MSbuild FLExBridge.build.win.proj /target:Installer /p:Configuration=Debug /p:Platform="Any CPU" /p:teamcity_build_checkoutDir=..\  /p:teamcity_dotnet_nunitlauncher_msbuild_task="notthere" /p:BUILD_NUMBER="0.3.1" /p:Minor="1" /p:BUILD_VCS_NUMBER=0
 popd
 PAUSE
