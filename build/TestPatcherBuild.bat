edd63feefb91c8d41fb78d18aaad9bbc54d31b35
Hasso Pape <hasso_pape@sil.org>
2017-11-01 16:52:33 -0500

    Set base version for patches (hard-coded 4th segment)

---
+++ build/TestPatcherBuild.bat
@@ -0,0 +1,12 @@
+REM This script assumes you have already downloaded TeamCity dependencies
+
+setlocal
+call "c:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"
+
+pushd .
+(
+	MSBuild FLExBridge.proj /target:RestorePackages
+) && (
+	MSBuild FLExBridge.proj /target:Patcher /p:Configuration=Debug /p:Platform="Any CPU" /p:BUILD_NUMBER="0.3.1" /p:Minor="1" /p:BUILD_VCS_NUMBER=0
+)
+popd
