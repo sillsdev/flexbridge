5b040ac9acff403b8aa158cad0b4d2e14f19ca16
Hasso Pape <hasso_pape@sil.org>
2018-03-21 16:21:31 -0500

    Build with Visual Studio 2017

--- build/build.bat
+++ build/build.bat
@@ -1,11 +1,16 @@
 REM This script assumes you have already downloaded TeamCity dependencies
 setlocal
-call "%VS140COMNTOOLS%vsvars32.bat"
+if not "%VS140COMNTOOLS%" == "" (
+	call "%VS140COMNTOOLS%vsvars32.bat"
+) else (
+	REM TODO (Hasso) 2018.03 (LT-19015): use a more-principaled way to find Visual Studio, like the Locator NuGet package
+	call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars32.bat"
+)
 
 pushd .
 (
 	MSBuild FLExBridge.proj /target:RestorePackages
 ) && (
-	MSBuild FLExBridge.proj %*
+	MSBuild FLExBridge.proj /p:Platform="Any CPU" %*
 )
 popd
