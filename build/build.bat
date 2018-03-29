a7e8b08f685b236128d07930c4bb65169ad029c7
Raymond Luong <raymond_luong@sil.org>
2018-03-29 12:26:51 -0600

    Use vswhere to locate MSBuild tool version 15.0

--- build/build.bat
+++ build/build.bat
@@ -1,12 +1,17 @@
 REM This script assumes you have already downloaded TeamCity dependencies
 setlocal
-if not "%VS140COMNTOOLS%" == "" (
+if not "%VS140COMNTOOLS%"=="" (
 	call "%VS140COMNTOOLS%vsvars32.bat"
-) else (
-	REM TODO (Hasso) 2018.03 (LT-19015): use a more-principaled way to find Visual Studio, like the Locator NuGet package
-	call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars32.bat"
+	GOTO Build
+)
+for /f "usebackq delims=" %%i in (`vswhere -latest -requires Microsoft.Component.MSBuild -property installationPath`) do (
+	set InstallDir=%%i
 )
 
+if "%arch%" == "" set arch=x86
+call "%InstallDir%\VC\Auxiliary\Build\vcvars32.bat" %arch% 8.1 -vcvars_ver=14.0
+
+:Build
 pushd .
 (
 	MSBuild FLExBridge.proj /target:RestorePackages
