5eeb849a8a8dfbfcfb7a73b4d7caf305de004c17
papeh <hasso_pape@sil.org>
2018-04-04 16:04:38 -0500

    Build with Visual Studio 2017 (#216)
    * Use vswhere to locate MSBuild tool version 15.0
    * Accept Visual Studio 2017's changes to project files

--- build/build.bat
+++ build/build.bat
@@ -1,11 +1,20 @@
 REM This script assumes you have already downloaded TeamCity dependencies
 setlocal
-call "%VS140COMNTOOLS%vsvars32.bat"
+if not "%VS140COMNTOOLS%"=="" (
+	call "%VS140COMNTOOLS%vsvars32.bat"
+	GOTO Build
+)
 
+for /f "usebackq delims=" %%i in (`vswhere -latest -requires Microsoft.Component.MSBuild -property installationPath`) do (
+	set InstallDir=%%i
+)
+call "%InstallDir%\VC\Auxiliary\Build\vcvars32.bat"
+
+:Build
 pushd .
 (
 	MSBuild FLExBridge.proj /target:RestorePackages
 ) && (
-	MSBuild FLExBridge.proj %*
+	MSBuild FLExBridge.proj /p:Platform="Any CPU" %*
 )
 popd
