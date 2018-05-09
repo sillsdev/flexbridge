e7f813dee49f739e9caa133f94cd149b0f9195c6
sam <sam147doss@gmail.com>
2018-05-09 19:50:04 +0530

    Fix: LT-18748 FLExBridge installer should use the Chorus icon
     * Added the icon file for the FLExBridge.

--- build/TestInstallerBuild.bat
+++ build/TestInstallerBuild.bat
@@ -1,7 +1,16 @@
 REM This script assumes you have already downloaded TeamCity dependencies
 
 setlocal
-call "%VS140COMNTOOLS%vsvars32.bat"
+
+if "%arch%" == "" set arch=x86
+
+for /f "usebackq tokens=1* delims=: " %%i in (`vswhere -latest -requires Microsoft.Component.MSBuild`) do (
+  if /i "%%i"=="installationPath" set InstallDir=%%j
+)
+
+call "%InstallDir%\VC\Auxiliary\Build\vcvarsall.bat" %arch% 8.1
+
+set Path=%WIX%\bin;%PATH%
 
 pushd .
 (
