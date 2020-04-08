25e538ebb776cc34fdf1b20cf4abde0cd02b4664
Jason Naylor <jason_naylor@sil.org>
2020-04-08 11:37:17 -0500

    Localize FlexBridge with Crowdin
    * Use xliff with L10nSharp
    * Consume Nuget Packages for libpalaso and Chorus
    * Download FlexBridge l10ns from FieldWorks Crowdin
    * Update TestInstaller.bat task to grab latest l10ns
    * Remove .tmx files from repo

--- build/TestInstallerBuild.bat
+++ build/TestInstallerBuild.bat
@@ -1,13 +1,29 @@
 REM This script assumes you have already downloaded TeamCity dependencies
-
 setlocal
-call "%VS140COMNTOOLS%vsvars32.bat"
+if not "%VS140COMNTOOLS%"=="" (
+	call "%VS140COMNTOOLS%vsvars32.bat"
+	GOTO Build
+)
+
+for /f "delims=" %%i in ('vswhere -nologo -version "[15.0,16.0)" -requires Microsoft.Component.MSBuild -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath') do (
+	set InstallDir=%%i
+)
+call "%InstallDir%\VC\Auxiliary\Build\vcvars32.bat"
+
+:Build
 
 set Path=%WIX%\bin;%PATH%
-
+echo Starting Build
 pushd .
 (
-	MSBuild FLExBridge.proj /target:RestorePackages
+	MSBuild FLExBridge.proj /t:RestoreBuildTasks;RestorePackages
+) && (
+	pushd ..\l10n
+) && (
+    MSBuild l10n.proj /t:restore
+	MSBuild l10n.proj /t:GetLatestL10ns
+) && (
+	popd
 ) && (
 	MSBuild FLExBridge.proj /target:Installer /p:Configuration=Debug /p:Platform="Any CPU" %*
 )
