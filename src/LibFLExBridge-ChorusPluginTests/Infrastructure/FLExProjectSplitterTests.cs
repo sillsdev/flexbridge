a9a799bc1b218a054a8493ce860e8e8b7b46f4c0
Jason Naylor <jasonleenaylor@users.noreply.github.com>
2020-11-24 14:15:20 -0800

    Clear removed project settings from the cache (#295)
    * Fix LT-20464 - Remove project settings files
      from the cache if they are no longer in the
      source folders

--- src/LibFLExBridge-ChorusPluginTests/Infrastructure/FLExProjectSplitterTests.cs
+++ src/LibFLExBridge-ChorusPluginTests/Infrastructure/FLExProjectSplitterTests.cs
@@ -1,13 +1,16 @@
-﻿// Copyright (c) 2010-2016 SIL International
+// Copyright (c) 2010-2016 SIL International
 // This software is licensed under the MIT License (http://opensource.org/licenses/MIT)
 
 using System;
+using System.IO;
 using Chorus.Utilities;
+using LibFLExBridgeChorusPlugin.Contexts;
 using LibFLExBridgeChorusPlugin.DomainServices;
 using LibTriboroughBridgeChorusPlugin;
 using NUnit.Framework;
 using SIL.IO;
 using SIL.Progress;
+using SIL.TestUtilities;
 
 namespace LibFLExBridgeChorusPluginTests.Infrastructure
 {
@@ -53,6 +56,43 @@ namespace LibFLExBridgeChorusPluginTests.Infrastructure
 				};
 				var pathname = tempFile.Path;
 				Assert.Throws<UserCancelledException>(() => FLExProjectSplitter.PushHumptyOffTheWall(progress, false, pathname));
+			}
+		}
+
+		[Test]
+		public void CopySupportingSettingsFilesIntoRepo_WritingSystemsCopiedToCache()
+		{
+			// Setup a folder with a writing system to copy and an empty cache folder.
+			using (var tempProjFolder = new TemporaryFolder("WritingSystemsCopiedToCache"))
+			{
+				var wsFolder = Path.Combine(tempProjFolder.Path, "WritingSystemStore");
+				var wsCacheFolder = Path.Combine(tempProjFolder.Path, "CachedSettings", "WritingSystemStore");
+				Directory.CreateDirectory(wsFolder);
+				File.WriteAllText(Path.Combine(wsFolder, "en.ldml"), "<ldml/>");
+				Assert.IsFalse(Directory.Exists(wsCacheFolder));
+				// SUT
+				BaseDomainServices.CopySupportingSettingsFilesIntoRepo(new NullProgress(), false, tempProjFolder.Path);
+				// Verify that the writing system was copied into the cache.
+				Assert.IsTrue(File.Exists(Path.Combine(wsCacheFolder, "en.ldml")));
+			}
+		}
+
+		[Test]
+		public void CopySupportingSettingsFilesIntoRepo_DeletedWritingSystemsRemovedFromCache()
+		{
+			// Setup an empty project writing system store and a cache containing one writing system.
+			using (var tempProjFolder = new TemporaryFolder("DeletedWritingSystemsRemovedFromCache"))
+			{
+				var wsFolder = Path.Combine(tempProjFolder.Path, "WritingSystemStore");
+				var wsCacheFolder = Path.Combine(tempProjFolder.Path, "CachedSettings", "WritingSystemStore");
+				Directory.CreateDirectory(wsFolder);
+				Directory.CreateDirectory(wsCacheFolder);
+				File.WriteAllText(Path.Combine(wsCacheFolder, "en.ldml"), "<ldml/>");
+				Assert.IsFalse(File.Exists(Path.Combine(wsFolder, "en.ldml")));
+				// SUT
+				BaseDomainServices.CopySupportingSettingsFilesIntoRepo(new NullProgress(), false, tempProjFolder.Path);
+				// Verify that the writing system was deleted from the cache
+				Assert.IsFalse(File.Exists(Path.Combine(wsCacheFolder, "en.ldml")));
 			}
 		}
 	}
