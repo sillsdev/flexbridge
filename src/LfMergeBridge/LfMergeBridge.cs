e94d6db1889eafadd3405a65c17dc86b11663e03
Robin Munn <rmunn@pobox.com>
2024-04-24 10:15:31 +0700

    Switch name of fwdata file API
    The Humpty name is fun and cleverly silly, but when we expose it to
    outside code in LfMerge, it might be nicer to have a name that
    communicates its purpose a little more clearly.

--- src/LfMergeBridge/LfMergeBridge.cs
+++ src/LfMergeBridge/LfMergeBridge.cs
@@ -72,7 +72,12 @@ namespace LfMergeBridge
 			return true;
 		}
 
-		public static void PutHumptyTogetherAgain(IProgress progress, bool writeVerbose, string mainFilePathname)
+		public static void DisassembleFwdataFile(IProgress progress, bool writeVerbose, string mainFilePathname)
+		{
+			LibFLExBridgeChorusPlugin.DomainServices.FLExProjectSplitter.PushHumptyOffTheWall(progress, writeVerbose, mainFilePathname);
+		}
+
+		public static void ReassembleFwdataFile(IProgress progress, bool writeVerbose, string mainFilePathname)
 		{
 			LibFLExBridgeChorusPlugin.DomainServices.FLExProjectUnifier.PutHumptyTogetherAgain(progress, writeVerbose, mainFilePathname);
 		}
