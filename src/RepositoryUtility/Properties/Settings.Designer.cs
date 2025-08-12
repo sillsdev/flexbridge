909f1c930020bd3ab156c4fffab5fbdfcfa7c053
Jason Naylor <jason_naylor@sil.org>
2025-08-12 10:58:05 -0700

    Improve workflow for debugging a Send/Receive

--- src/RepositoryUtility/Properties/Settings.Designer.cs
+++ src/RepositoryUtility/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace RepositoryUtility.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.10.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.14.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -70,6 +70,18 @@ namespace RepositoryUtility.Properties {
             }
             set {
                 this["CallUpgrade"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string OpenLocalStartFolder {
+            get {
+                return ((string)(this["OpenLocalStartFolder"]));
+            }
+            set {
+                this["OpenLocalStartFolder"] = value;
             }
         }
     }
